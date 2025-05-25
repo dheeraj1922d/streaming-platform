30c2aa36c2a6cabce6a7510288014105071172aa
Dhiraj <rd6999787@gmail.com>
2025-05-25 19:37:57 +0530

    s3 to transcode to s3

--- transcoder_service/index.js
+++ transcoder_service/index.js
@@ -1,31 +1,44 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import KafkaConfig from "../upload_service/kafka/kafka.js";
 
-
 dotenv.config();
-const port = 8081
+const port = 8081;
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     allowedHeaders: ["*"],
-    origin: "*"
-}));
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Transcoder service is working')
-})
+app.get("/", (req, res) => {
+  res.send("Transcoder service is working");
+});
 
-
-const kafkaconfig = new KafkaConfig()
-kafkaconfig.consume("transcode", (value) => {
-    console.log("Got data from kafka : ", value)
-})
-
+const kafkaconfig = new KafkaConfig();
+kafkaconfig.consume("transcode", async (message) => {
+  try {
+    console.log("Got data from Kafka:", message);
+    // Parsing JSON message value
+    const value = JSON.parse(message);
+    // Checking if value and filename exist
+    if (value && value.filename) {
+      console.log("Filename is", value.filename);
+      await s3ToS3(value.filename); // Make this change in controller
+    } else {
+      console.log("Didn't receive filename to be picked from S3");
+    }
+  } catch (error) {
+    console.error("Error processing Kafka message:", error);
+    // You might want to handle or log this error appropriately
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
-})
+  console.log(`Server is listening at http://localhost:${port}`);
+});
